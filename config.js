5852c6c9d818cbf4cc38592b325aac3423ee2006
shandrai <shandrai@gmail.com>
2016-08-02 04:52:38 +0000

    Update config with mLab URI

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
+                            'mongodb://thinkful:thinkful@ds139735.mlab.com:39735/thinkful_shopping_list' :
                             'mongodb://localhost/shopping-list-dev');
                             
 exports.PORT = process.env.PORT || 8080;
